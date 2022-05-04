2349f0c3892d17449891114ba04ebd7590bb15fb
helloworld3200 <92115307+helloworld3200@users.noreply.github.com>
2022-05-04 18:56:56 +0100

    c

--- bot.js
+++ bot.js
@@ -5,10 +5,11 @@ var question_real = questiont.replace("×", "*").replace("÷", "/");
 var answer = String(eval(question_real));
 console.log(answer);
 console.log(answer.length);
-for (var i = 0; answer.length > i; i++) {
-console.log("in the thing");
-var answerid = String(answer.charAt(i))
-console.log(answerid)
-document.getElementById("number-"+answerid).click();
-};
+document.getElementById("calculator-display").value += answer
+//for (var i = 0; answer.length > i; i++) {
+//console.log("in the thing");
+//var answerid = "number-"+String(answer.charAt(i))
+//console.log(answerid)
+//document.getElementById("number-"+answerid).click();
+//};
 };
