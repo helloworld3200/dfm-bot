fb12ece5bddd3893960fb7a123be87cec52ac582
helloworld3200 <92115307+helloworld3200@users.noreply.github.com>
2022-05-04 19:09:43 +0100

    c

--- bot.js
+++ bot.js
@@ -1,4 +1,34 @@
 function runmain() {
+Podium = {};
+Podium.keydown = function(k) {
+    var oEvent = document.createEvent('KeyboardEvent');
+
+    // Chromium Hack
+    Object.defineProperty(oEvent, 'keyCode', {
+                get : function() {
+                    return this.keyCodeVal;
+                }
+    });     
+    Object.defineProperty(oEvent, 'which', {
+                get : function() {
+                    return this.keyCodeVal;
+                }
+    });     
+
+    if (oEvent.initKeyboardEvent) {
+        oEvent.initKeyboardEvent("keydown", true, true, document.defaultView, false, false, false, false, k, k);
+    } else {
+        oEvent.initKeyEvent("keydown", true, true, document.defaultView, false, false, false, false, k, 0);
+    }
+
+    oEvent.keyCodeVal = k;
+
+    if (oEvent.keyCode !== k) {
+        alert("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ")");
+    }
+
+    document.dispatchEvent(oEvent);
+}  
 var question = document.getElementById("question");
 var questiont = question.innerHTML;
 var question_real = questiont.replace("×", "*").replace("÷", "/");
@@ -6,7 +36,7 @@ var answer = String(eval(question_real));
 console.log(answer);
 console.log(answer.length);
 document.getElementById("calculator-display").value += answer
-document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': '13'}));
+Podium.keydown(13)
 //for (var i = 0; answer.length > i; i++) {
 //console.log("in the thing");
 //var answerid = "number-"+String(answer.charAt(i))
