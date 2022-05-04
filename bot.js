f0b1498802db29db14b931ec041e1d213f263382
helloworld3200 <92115307+helloworld3200@users.noreply.github.com>
2022-05-04 19:00:10 +0100

    c

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ var answer = String(eval(question_real));
 console.log(answer);
 console.log(answer.length);
 document.getElementById("calculator-display").value += answer
+document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': '13'}));
 //for (var i = 0; answer.length > i; i++) {
 //console.log("in the thing");
 //var answerid = "number-"+String(answer.charAt(i))
