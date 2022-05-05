c4a410a71ce1c7f2813e7b6c98e4807d2f82e96b
helloworld3200 <92115307+helloworld3200@users.noreply.github.com>
2022-05-05 11:08:33 +0100

    c

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+
 function runmain() {
 var question = document.getElementById("question");
 var questiont = question.innerHTML;
@@ -7,11 +8,30 @@ console.log(answer);
 console.log(answer.length);
 document.getElementById("calculator-display").value += answer
 
+let TargetInput = document.getElementById('calculator-display')
+
+TargetInput.addEventListener('keydown',function(e){
+    if(!e.isTrusted){
+		//Mannually triggered
+		this.value += e.data;
+	}
+}, false);
+
+function Trigger(){
+	var event = new KeyboardEvent('keydown', {
+    keyCode : "13"
+	}); 
+
+	TargetInput.dispatchEvent(event);
+}
+
+Trigger()
+
 //for (var i = 0; answer.length > i; i++) {
 //console.log("in the thing");
 //var answerid = "number-"+String(answer.charAt(i))
 //console.log(answerid)
 //document.getElementById("number-"+answerid).click();
 //};
-startbot.addEventListener("click", {runmain()})
+
 };
